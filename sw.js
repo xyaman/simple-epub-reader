e8d7a204306ada58f2adf538efafb7841ee08c82
xyaman <dbarriapaiva@gmail.com>
2024-11-13 03:03:48 +0000

    Deploying to gh-pages from @ xyaman/simple-epub-reader@3bfa5ef16769c599387b6d9c18fba79c99c0597f 🚀

--- sw.js
+++ sw.js
@@ -1,18 +1,26 @@
 const CACHE_NAME = "epub-reader";
 
 // A change of one character should trigger the update 
-const version = "0.0.1-v2"
+const version = "0.0.1-v3"
 
 const urlsToCache = [
   "/",
   "/sw.js",
-  "/style.css",
+
+  "/css/style.css",
+  "/css/font-awesome.min.css",
+  "/css/bulma.min.css",
+  "/webfonts/fa-solid-900.ttf",
+  "/webfonts/fa-solid-900.woff2",
+
   "/index.html",
   "/reader.html",
   "/settings.html",
+
   "/js/collection.js",
   "/js/reader.js",
   "/js/settings.js",
+  "/js/shared.js",
 ];
 
 // Instalar el service worker y cachear los recursos
@@ -39,11 +47,18 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// Interceptar solicitudes de red y servir desde el caché si está disponible
 self.addEventListener("fetch", (event) => {
+  const requestURL = new URL(event.request.url);
+
+  // reader.html always has a param id
+  // ..../reader.html?id=1
+  // ..../reader.html?id=3
+  // we are going to ignore those by only using pathname
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(requestURL.pathname).then((response) => {
       return response || fetch(event.request);
     })
   );
+
 });
